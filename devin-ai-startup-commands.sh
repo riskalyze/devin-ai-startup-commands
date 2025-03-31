f693fa0d32fd0841eb83c22ad1079d56791edc4b
Montana Payne <mpayne@nitrogenwealth.com>
2025-03-31 11:17:04 -0700

    fix(commands): force overwrite

--- devin-ai-startup-commands.sh
+++ devin-ai-startup-commands.sh
@@ -7,7 +7,7 @@ DEBIAN_FRONTEND=noninteractive
 sudo apt update
 sudo apt install curl unzip -y
 curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
-unzip -u awscliv2.zip
+unzip -o awscliv2.zip
 sudo ./aws/install --bin-dir /usr/bin --install-dir /usr/local/aws-cli --update
 # secrets in devin.ai env
 aws configure set aws_access_key_id $AWS_ACCESS_KEY_ID
