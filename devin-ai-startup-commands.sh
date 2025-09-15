d884380b5f1e86d6613fe16bfc4edd78d94687dd
Montana Payne <mpayne@nitrogenwealth.com>
2025-09-15 13:15:19 -0700

    feat: twingate dev access

--- devin-ai-startup-commands.sh
+++ devin-ai-startup-commands.sh
@@ -24,3 +24,41 @@ then
   mise trust -a
 fi
 mise install
+
+echo "deb [trusted=yes] https://packages.twingate.com/apt/ /" | sudo tee /etc/apt/sources.list.d/twingate.list
+sudo apt-get update -o Dir::Etc::sourcelist="sources.list.d/twingate.list" -o Dir::Etc::sourceparts="-" -o APT::Get::List-Cleanup="0"
+sudo apt install -yq twingate
+echo "${DEVIN_TWINGATE_ACCESS}" | sudo twingate setup --headless -
+MAX_RETRIES=5
+WAIT_TIME=5
+n=0
+
+while [ $n -lt $MAX_RETRIES ]; do
+  echo "Starting Twingate service..."
+  set +xe
+  twingate start
+
+  echo "Waiting $WAIT_TIME seconds for Twingate service to start..."
+  sleep $WAIT_TIME
+
+  status=$(twingate status)
+  echo "Twingate service status: '$status'"
+
+  if [ "$status" = "online" ]; then
+    echo "Twingate service is connected."
+    break
+  else
+    twingate stop
+  fi
+
+  # Increment the retry counter and wait time
+  n=$((n+1))
+  WAIT_TIME=$((WAIT_TIME+5))
+
+  echo "Twingate service is not connected. Retrying ..."
+done
+
+if [ $n -eq $MAX_RETRIES ]; then
+  echo "Twingate service failed to connect."
+  exit 1
+fi
