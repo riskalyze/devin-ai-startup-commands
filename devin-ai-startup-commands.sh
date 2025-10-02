c2df67eb09ca53ac0d526af7301033eee614a1ec
Montana Payne <mpayne@nitrogenwealth.com>
2025-10-02 13:16:23 -0700

    fix: hide account

--- devin-ai-startup-commands.sh
+++ devin-ai-startup-commands.sh
@@ -9,12 +9,14 @@ sudo apt install curl unzip -y
 curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
 unzip -o awscliv2.zip
 sudo ./aws/install --bin-dir /usr/bin --install-dir /usr/local/aws-cli --update
+
 # secrets in devin.ai env
 aws configure set aws_access_key_id $AWS_ACCESS_KEY_ID
 aws configure set aws_secret_access_key $AWS_SECRET_ACCESS_KEY
 aws configure set default.region us-east-2
-aws codeartifact login --tool npm --domain riskalyze --domain-owner 125149417810 --namespace rsk --repository npm
-# aws ecr get-login-password --region us-east-2 | docker login --username AWS --password-stdin 125149417810.dkr.ecr.us-east-2.amazonaws.com  
+aws codeartifact login --tool npm --domain riskalyze --domain-owner $AWS_SHARED_ACCOUNT_ID --namespace rsk --repository npm
+aws ecr get-login-password --region us-east-2 | docker login --username AWS --password-stdin $AWS_SHARED_ACCOUNT_ID.dkr.ecr.us-east-2.amazonaws.com
+
 popd
 
 if [ -z $(which mise) ] 
